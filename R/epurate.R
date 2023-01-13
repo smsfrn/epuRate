9e34fcd959dec64066f8f905c829ac2344280830
Sam Safran <61366601+smsfrn@users.noreply.github.com>
2023-01-12 23:40:44 -0600

    icon work; toc-depth

--- R/epurate.R
+++ R/epurate.R
@@ -66,7 +66,7 @@ UQ <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
 
 
 # Features of QIMR Template
-QIMR <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+QIMR <- function(toc = TRUE, toc_depth = 2, code_folding = "hide", number_sections=TRUE) {
 
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "QIMR" ,"resources", "style.css", package = "epuRate")
@@ -83,26 +83,4 @@ QIMR <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                df_print = "paged",
                                code_folding = code_folding,
                             )
-}
-
-# Features of EAFIT Template
-EAFIT <- function(toc = TRUE, code_folding = "show", number_sections = TRUE) {
-
-  # get the locations of resource files located within the package
-  css <- system.file("rmarkdown", "templates", "EAFIT" ,"resources", "style.css", package = "epuRate")
-  template <- system.file("rmarkdown", "templates", "EAFIT" ,"resources", "template_eafit.html", package = "epuRate")
-  bibliography <- system.file("rmarkdown", "templates", "EAFIT" ,"resources", "bibliography.bib", package = "epuRate")
-
-  # call the base html_document function
-  rmarkdown::html_document( theme= "lumen",
-                            template = template,
-                            css = css,
-                            bibliography = bibliography,
-                            toc = toc,
-                            toc_float = TRUE,
-                            toc_depth = 2,
-                            number_sections = number_sections,
-                            df_print = "paged",
-                            code_folding = code_folding,
-  )
 }
