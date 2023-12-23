1ad5aa48d8c95a122df3433e36746a5c5119782e
Sam Safran <61366601+smsfrn@users.noreply.github.com>
2023-12-23 03:34:19 -0600

    template for site

--- R/epurate.R
+++ R/epurate.R
@@ -20,6 +20,25 @@ epurate <- function(toc = TRUE, toc_depth = 2, code_folding = "hide", number_sec
                             )
 }
 
+# Features of the Epurate Template
+epurate_site <- function(toc = TRUE, toc_depth = 2, code_folding = "hide", number_sections=TRUE) {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "epurate_site" ,"resources", "style.css", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "epurate_site" ,"resources", "template_epurate.html", package = "epuRate")
+
+  # call the base html_document function
+  rmarkdown::html_document( theme= "lumen",
+                            template= template,
+                            css= css,
+                            toc= toc,
+                            toc_float = TRUE,
+                            toc_depth = toc_depth,
+                            number_sections= number_sections,
+                            df_print = "paged",
+                            code_folding = code_folding,
+  )
+}
 
 # Features of PCTG Template
 PCTG <- function(toc = TRUE, toc_depth = 2, code_folding = "hide", number_sections=TRUE) {
